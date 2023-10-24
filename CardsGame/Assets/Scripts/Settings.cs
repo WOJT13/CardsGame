70f0ffddf7ffc965b14b886ecb166ff1df5f9f2b
WOJT13 <wojtekpalka123@gmail.com>
2023-10-24 17:12:34 +0200

    Second lab

---
+++ CardsGame/Assets/Scripts/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public static string playerNickname;
+
+    public Text nickname;
+
+    void Start()
+    {
+        nickname.text = playerNickname;
+    }
+}
